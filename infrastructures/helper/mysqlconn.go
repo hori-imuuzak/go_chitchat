e5ceb2fc0fb4b1eb87cd38a91f3dca7b4b14ea4b
堀知海 <kazuumi.hori@spacemarket.co.jp>
2020-07-04 13:58:48 +0900

    LogMode追加

--- infrastructures/helper/mysqlconn.go
+++ infrastructures/helper/mysqlconn.go
@@ -6,7 +6,14 @@ import (
 )
 
 func DBOpen() (*gorm.DB, error) {
-	return gorm.Open("mysql", "user:password@/master?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "user:password@/master?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		return nil, err
+	}
+
+	db.LogMode(true)
+
+	return db, nil
 }
 
 func DBClose(db *gorm.DB) {
